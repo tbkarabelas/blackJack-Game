3e9dbade46b93f9af9c3cdb0f2239668f68fafb1
tkarabelas <thomas@karabelas.com>
2026-02-01 18:32:44 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+let cardOne = 1;
+
+let cardTwo = 15;
+
+let cardTotal =  cardOne + cardTwo;
+
+let blackJackMessage = "";
+
+let hasBlackJack = false;
+let isAlive = true;
+
+let messageEl = document.getElementById("message-el");
+// console.log(messageEl);
+
+function startGame(){
+    if (cardTotal <= 20){
+    
+    blackJackMessage = "Hit or Stand?";
+   // messageEl.innerText = blackJackMessage+ ' ' + cardTotal;
+}
+else if(cardTotal === 21){
+    blackJackMessage = "Winner, Winner!! Chicken Dinner!!";
+    hasBlackJack = true;
+    // messageEl.innerText = blackJackMessage + ' ' + cardTotal;
+    
+}
+else {
+    blackJackMessage = "Busted:";
+    isAlive = false;
+    // messageEl.innerText = blackJackMessage + " " + cardTotal;
+};
+
+messageEl.textContent = blackJackMessage + " " + cardTotal;
+// console.log(blackJackMessage);
+}
+
+
+// console.log(hasBlackJack);
+// console.log(isAlive);
+
+
